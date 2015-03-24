fb6df6ccc5e2d6817c1e026ddb82c21d1dc19cf8
Godley <charlotte.godley@hotmail.co.uk>
2015-03-24 20:42:11 +0000

    work toward issue #9
    - created methods to write Ndef message
    - tested with reader app confirms it works

--- app/src/main/java/mobiledev/charlottegodley/gemmaphoneapp/ColorPickerActivity.java
+++ app/src/main/java/mobiledev/charlottegodley/gemmaphoneapp/ColorPickerActivity.java
@@ -1,24 +1,47 @@
 package mobiledev.charlottegodley.gemmaphoneapp;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.nfc.FormatException;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 public class ColorPickerActivity extends Activity implements
         ColorPicker.OnColorChangedListener {
+    NfcAdapter adapter;
+    PendingIntent pendingIntent;
+    IntentFilter writeTagFilters[];
+    boolean writeMode;
+    Tag mytag;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_picker);
         this.activity = this;
+
+        adapter = NfcAdapter.getDefaultAdapter(this);
+        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
     }
 
     @Override
@@ -71,13 +94,79 @@ public class ColorPickerActivity extends Activity implements
                     finally {
                         nfcString.append(colString);
                         nfcString.append(secondsInt);
-                        nfcString.append(".");
                     }
                 }
 
 
             }
             Log.i("output", nfcString.toString());
+            enableWriteMode();
+            try {
+                if(mytag==null){
+                    Toast.makeText(this, "ERROR detected", Toast.LENGTH_LONG).show();
+
+                }else{
+                    write(nfcString.toString(),mytag);
+                    Toast.makeText(this, "NFC write ok", Toast.LENGTH_LONG ).show();
+                }
+            } catch (IOException e) {
+                Toast.makeText(this, "Error writing NFC", Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            } catch (FormatException e) {
+                Toast.makeText(this, "Error writing NFC", Toast.LENGTH_LONG ).show();
+                e.printStackTrace();
+            }
+
         }
+
+    }
+
+    private NdefRecord createRecord(String text) throws UnsupportedEncodingException {
+
+        //create the message in according with the standard
+        String lang = "en";
+        byte[] textBytes = text.getBytes();
+        byte[] langBytes = lang.getBytes("US-ASCII");
+        int langLength = langBytes.length;
+        int textLength = textBytes.length;
+
+        byte[] payload = new byte[1 + langLength + textLength];
+        payload[0] = (byte) langLength;
+
+        // copy langbytes and textbytes into payload
+        System.arraycopy(langBytes, 0, payload, 1, langLength);
+        System.arraycopy(textBytes, 0, payload, 1 + langLength, textLength);
+
+        NdefRecord recordNFC = new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, new byte[0], payload);
+        return recordNFC;
+    }
+
+    private void write(String text, Tag tag) throws IOException, FormatException {
+
+        NdefRecord[] records = { createRecord(text) };
+        NdefMessage message = new NdefMessage(records);
+        Ndef ndef = Ndef.get(tag);
+        ndef.connect();
+        ndef.writeNdefMessage(message);
+        ndef.close();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
+            mytag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            Toast.makeText(this, "Tag detected " + mytag.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void enableWriteMode(){
+        writeMode = true;
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        IntentFilter[] filters = new IntentFilter[] { tagDetected };
+
+        adapter.enableForegroundDispatch(this, pendingIntent, filters, null);
     }
 }
