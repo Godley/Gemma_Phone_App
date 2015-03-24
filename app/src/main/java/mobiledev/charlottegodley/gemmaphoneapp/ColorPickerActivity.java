b03a0312996fba82d405aab1d40f85267ccae10e
Godley <charlotte.godley@hotmail.co.uk>
2015-03-24 18:32:17 +0000

    connected NFC button to logger callback
    - upon nfc button press for loop combines button colour with seconds input
    - this creates a string of all 16 LEDs, giving each one 8 hex digits (the first 6 a colour, the last 2 number of seconds)
    - issue fix #4

--- app/src/main/java/mobiledev/charlottegodley/gemmaphoneapp/ColorPickerActivity.java
+++ app/src/main/java/mobiledev/charlottegodley/gemmaphoneapp/ColorPickerActivity.java
@@ -2,8 +2,14 @@ package mobiledev.charlottegodley.gemmaphoneapp;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ColorPickerActivity extends Activity implements
         ColorPicker.OnColorChangedListener {
@@ -25,5 +31,53 @@ public class ColorPickerActivity extends Activity implements
 
     public void getColor(View v) {
         new ColorPicker(activity, this, v.getId(), "", Color.BLACK, Color.WHITE).show();
+    }
+
+    public void nfcButtonClick(View v) {
+        StringBuilder nfcString = new StringBuilder();
+        if(v.getId() == R.id.nfcButton) {
+
+            int leds[] = {R.id.led1color,R.id.led2color,R.id.led3color,R.id.led4color,
+                                    R.id.led5color,R.id.led6color, R.id.led7color,R.id.led8color,
+                                    R.id.led9color,R.id.led10color,R.id.led11color,R.id.led12color,
+                                    R.id.led13color,R.id.led14color, R.id.led15color,R.id.led16color};
+            int seconds[] = {R.id.ledSeconds1,R.id.ledSeconds2,R.id.ledSeconds3,R.id.ledSeconds4,
+                    R.id.ledSeconds5,R.id.ledSeconds6, R.id.ledSeconds7,R.id.ledSeconds8,
+                    R.id.ledSeconds9,R.id.ledSeconds10,R.id.ledSeconds11,R.id.ledSeconds12,
+                    R.id.ledSeconds13,R.id.ledSeconds14, R.id.ledSeconds15,R.id.ledSeconds16};
+            for(int i=0;i<seconds.length;i++) {
+                View btn = ColorPickerActivity.this.findViewById(leds[i]);
+                String colString = "";
+                try {
+                    ColorDrawable buttonColor = (ColorDrawable) btn.getBackground();
+                    if(buttonColor != null) {
+                        int colorId = buttonColor.getColor();
+                        colString = Integer.toHexString(colorId);
+                    }
+                }
+                catch(ClassCastException e) {
+                    colString = "000000";
+                }
+                finally {
+                    EditText secondsInput = (EditText)ColorPickerActivity.this.findViewById(seconds[i]);
+                    int secondsInt = 0;
+                    String secondsString = secondsInput.getText().toString();
+                    try {
+                        secondsInt = Integer.parseInt(secondsString);
+                    }
+                    catch(NumberFormatException e) {
+                        secondsInt = 0;
+                    }
+                    finally {
+                        nfcString.append(colString);
+                        nfcString.append(Integer.toHexString(secondsInt));
+
+                    }
+                }
+
+
+            }
+            Log.i("output", nfcString.toString());
+        }
     }
 }
